141e2098c31412e0d83536f6020af38476af6b17
Andres Valencia <andres.valencia@appdirect.com>
2017-02-09 14:16:35 -0500

    fix indentation

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <router-link to="/dashboard">Go to Dashboard</router-link>
-
-    <h4>{{ data }}</h4>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
   data() {
     return {
-      msg: 'Expresso, the story mapping tool.',
       data: '',
     };
   },
@@ -24,34 +19,16 @@ export default {
   },
   methods: {
     fetchData() {
-      this.$http
-        .get('/api/2/issue/DS-12906').then((data) => {
-          this.data = data;
-        }, (err) => {
-          this.data = err;
-        });
+      this.$http.get('/api/2/issue/DS-12906').then((data) => {
+        this.data = data;
+      }, (err) => {
+        this.data = err;
+      });
     },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
