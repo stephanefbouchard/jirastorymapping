ba3b444fd7b0124a8df18c224b944eea5ef23e2a
Andres Valencia <andres.valencia@appdirect.com>
2017-02-09 13:52:26 -0500

    smaller logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/expresso.jpg">
+    <img src="./assets/expresso.jpg" class="brand">
     <router-view></router-view>
   </div>
 </template>
@@ -19,5 +19,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.brand {
+  max-width: 120px;
 }
 </style>
