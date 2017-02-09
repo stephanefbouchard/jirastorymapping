24ce538bd7f685e6ad598f49e19eaefd9a9a5602
Andres Valencia <andres.valencia@appdirect.com>
2017-02-09 14:51:48 -0500

    about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <img src="../assets/expresso.jpg" alt="expresso">
-    <p>Expresso is a tool for mapping Jira stories on the backlog</p>
+    <p>Expresso is a tool for mapping backlog Jira</p>
     <p>&copy; 2017</p>
   </div>
 </template>
