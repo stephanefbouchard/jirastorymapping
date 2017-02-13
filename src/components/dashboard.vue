be428ab4f2d667a85c2db3a0335451473d0ebb7d
Stephane Fortin Bouchard <stephane.fortinbouchard@appdirect.com>
2017-02-13 13:31:02 -0500

    Added vue-material

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <md-card md-with-hover>
+      <md-card-header>
+        <div class="md-title">Title goes here</div>
+        <div class="md-subhead">Subtitle here</div>
+      </md-card-header>
+
+      <md-card-content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button>Action</md-button>
+        <md-button>Action</md-button>
+      </md-card-actions>
+    </md-card>
+
     <h4>{{ data }}</h4>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Resource from 'vue-resource';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.css';
 import App from './App';
 import router from './router';
+
+Vue.use(Resource);
+Vue.use(VueMaterial);
+
+
+// VueMaterial.theme.register('default', {
+//  primary: 'blue',
+// });
+Vue.http.headers.common.Authorization = 'Basic c3RlcGhhbmUuZm9ydGluYm91Y2hhcmQ6UG9rZXB1Y2UxIQ==';
 
 /* eslint-disable no-new */
 new Vue({
