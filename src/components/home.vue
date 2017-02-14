ada7e82bd9c246a5c9b730d12de42f1b80c23734
Stephane Fortin Bouchard <stephane.fortinbouchard@appdirect.com>
2017-02-14 10:37:56 -0500

    Added the story point input

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,64 +1,12 @@
 <template>
-  <div class="login-form">
-      <form novalidate>
-        <h2>Login</h2>
-        <md-input-container>
-          <label>Username</label>
-          <md-input v-model="credentials.username"></md-input>
-        </md-input-container>
-
-        <md-input-container md-has-password>
-          <label>Password</label>
-          <md-input v-model="credentials.password" type="password"></md-input>
-        </md-input-container>
-
-        <md-button class="md-raised md-primary" type="submit" @click.native="doLogin">
-          Login
-        </md-button>
-      </form>
+  <div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      credentials: {
-        username: '',
-        password: '',
-      },
-    };
-  },
-  created() {
-    this.checkLogin();
-  },
-  methods: {
-    checkLogin() {
-      const credentials = this.$localStorage.get('credentials');
-      if (credentials) {
-        this.credentials = credentials;
-        this.doLogin();
-      }
-    },
-    doLogin() {
-      const token = btoa(`${this.credentials.username}:${this.credentials.password}`);
-      this.$http.headers.common.Authorization = `Basic ${token}`;
-
-      this.$http.get('/api/2/issue/createmeta').then(() => {
-        this.$localStorage.set('credentials', this.credntials);
-      }, () => {
-        console.log('error');
-      });
-    },
-  },
 };
 </script>
 
 <style scoped>
-  .login-form {
-    width: 300px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 100px;
-  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,16 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from 'components/home';
 import Dashboard from 'components/dashboard';
+import Login from 'components/login';
 import About from 'components/About';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
+  { path: '/', name: 'Login', component: Login },
   { path: '/dashboard/:id', name: 'Dashboard', component: Dashboard, props: true },
   { path: '/about', name: 'About', component: About },
+  { path: '/home', name: 'Home', component: Home },
 ];
 
 export default new VueRouter({ routes });
